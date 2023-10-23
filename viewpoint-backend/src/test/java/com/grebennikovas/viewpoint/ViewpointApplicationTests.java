480788621761b5c6e9415e703b1ebd95e1baccc0
grebennikov-undrew <grebennikovas@yahoo.com>
2023-10-23 18:20:00 +0300

    initial commit

---
+++ viewpoint-backend/src/test/java/com/grebennikovas/viewpoint/ViewpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grebennikovas.viewpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
