d1daec086bab6eb0f2fc85d896f53d294293d487
grebennikov-undrew <grebennikovas@yahoo.com>
2023-10-30 05:15:57 +0300

    added jwt basics

--- viewpoint-backend/src/main/java/com/grebennikovas/viewpoint/ViewpointApplication.java
+++ viewpoint-backend/src/main/java/com/grebennikovas/viewpoint/ViewpointApplication.java
@@ -3,10 +3,12 @@ package com.grebennikovas.viewpoint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class ViewpointApplication {
 
 	public static void main(String[] args) {
--- viewpoint-backend/src/main/java/com/grebennikovas/viewpoint/security/SecurityConfiguration.java
+++ viewpoint-backend/src/main/java/com/grebennikovas/viewpoint/security/SecurityConfiguration.java
@@ -1,17 +1,85 @@
 package com.grebennikovas.viewpoint.security;
 
+import com.grebennikovas.viewpoint.security.converters.PrivateKeyConverter;
+import com.grebennikovas.viewpoint.security.converters.PublicKeyConverter;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
 @Configuration
 @EnableWebSecurity
+@ConfigurationProperties(prefix = "jwt")
 public class SecurityConfiguration {
+
+    String publicKey;
+
+    String privateKey;
+
+    @Autowired
+    PublicKeyConverter publicKeyConverter;
+
+    @Autowired
+    PrivateKeyConverter privateKeyConverter;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests((authorize) -> authorize
+                    .anyRequest().authenticated()
+            )
+            .csrf((csrf) -> csrf.ignoringRequestMatchers("/token"))
+            .httpBasic(Customizer.withDefaults())
+            .oauth2ResourceServer((oauth2) -> oauth2
+                    .jwt(Customizer.withDefaults()))
+            .exceptionHandling((exceptions) -> exceptions
+                    .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
+                    .accessDeniedHandler(new BearerTokenAccessDeniedHandler())
+            )
+        ;
+        return http.build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(publicKeyConverter.convert(this.publicKey)).build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(publicKeyConverter.convert(this.publicKey)).privateKey(privateKeyConverter.convert(this.privateKey)).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
     }
 }
